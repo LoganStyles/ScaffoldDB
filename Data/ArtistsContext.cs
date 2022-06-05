76085e70ebec8a7b9472e85a9a748ffe0432ea12
LoganStyles <emmaist23@gmail.com>
2022-06-05 17:42:56 +0100

    feat(output directory): added output dir, created output/Artists.db, and removed Artists.db

--- Data/ArtistsContext.cs
+++ Data/ArtistsContext.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore.Metadata;
+﻿using Microsoft.EntityFrameworkCore;
 using ScaffoldDB.Entities;
 
 namespace ScaffoldDB.Data
@@ -27,7 +24,7 @@ namespace ScaffoldDB.Data
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlite("data source=Artists.db");
+                optionsBuilder.UseSqlite("data source=output/Artists.db");
             }
         }
 
